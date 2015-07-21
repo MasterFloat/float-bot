f3d41139a0520dd8d4c3cfb81bb73b9b4f4fd67c
MasterFloat <themasterfloat@gmail.com>
2015-07-21 08:12:19 +0200

    Config.js

---
+++ config.js
@@ -0,0 +1,27 @@
+exports.server = '158.69.1.126';
+exports.port = 7000;
+exports.serverid = 'eos';
+exports.nick = 'Your bot's name';
+exports.pass = 'Your bot's password';
+exports.rooms = ['room1', 'room2'];
+exports.privaterooms = ['private room1', 'private room2'];
+exports.commandcharacter = '.';
+exports.defaultrank = '@';
+exports.groups = ' +%@#&~';
+exports.watchconfig = false;
+exports.secprotocols = [];
+exports.debuglevel = 3;
+exports.excepts = ['yourusername'];
+exports.whitelist = ['yourusername'];
+exports.regexautobanwhitelist = ['yourusername'];
+exports.botguide = '';
+exports.fork = 'http://github.com/TalkTakesTime/Pokemon-Showdown-Bot';
+exports.allowmute = true;
+exports.punishvals = {
+	1: 'warn',
+	2: 'mute',
+	3: 'hourmute',
+	4: 'roomban',
+	5: 'ban'
+};
+exports.googleapikey = '';
